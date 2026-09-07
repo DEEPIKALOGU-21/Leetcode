c3b6330a3b4b43bddde6307b0ff41544f13d96d5
DEEPIKA U <deepikaulaganathan21@gmail.com>
2026-09-07 20:16:35 +0530

    Time: 3 ms (86.03%), Space: 43.3 MB (57.35%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,12 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
 
-        while (s.contains("()") || s.contains("[]") || s.contains("{}")) {
-            s = s.replace("()", "")
-                 .replace("[]", "")
-                 .replace("{}", "");
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
         }
 
-        return s.isEmpty();
+        return stack.isEmpty();
     }
 }
